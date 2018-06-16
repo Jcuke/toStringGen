8d3b7af14686af08406dd77cedeb3532bf3b0481
Jcuke <937000078@qq.com>
2018-06-16 18:37:29 +0800

    修复属性为自定义类型时，json值加了引号的问题

--- src/com/jcuke/tostringGen/ToStringGen.java
+++ src/com/jcuke/tostringGen/ToStringGen.java
@@ -14,6 +14,7 @@ import com.intellij.psi.util.PsiTreeUtil;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by cuke on 2018/6/16.
@@ -52,29 +53,32 @@ public class ToStringGen extends AnAction {
             fieldText = fieldText.replaceAll("  ", "").trim();
             String fieldClassType = fieldText.split(" ")[1];
 
-            boolean haveQuotationMark = true;
+            boolean isNumberType = false;
 
             //数组类型的属性值显示时不用加引号
             List<String> numberTypes = Arrays.asList(new String[]{"int", "float", "double", "short", "byte", "integer", "float"});
             for (String numberType : numberTypes) {
                 if(fieldClassType.equalsIgnoreCase(numberType)){
-                    haveQuotationMark = false;
+                    isNumberType = true;
                 }
             }
 
             String fname = field.getName();
 
-            //如果是字符串，需要加引号
-            if(haveQuotationMark){
+            if(isNumberType){
+                //数值类型的属性
+                sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ "\" + "+ fname +" +\"");
 
+            } else {
+                String classType = "java.util." + fieldClassType;
+                //如果不是数值类型，需要加引号
                 boolean isCollectionType = true;
                 try {
                     //对集合对象的处理
-                    String collectionsClassType = "java.util." + fieldClassType;
-                    Class fieldClass = Class.forName(collectionsClassType);
+                    Class fieldClass = Class.forName(classType);
                     //是否从Collections中继承
                     if(Collection.class.isAssignableFrom(fieldClass)){
-                        String printValue = "\"+("+ fname + " == null ? \"[]\" :\"[\" + StringUtils.join("+ fname +", \",\").replaceAll(\",\", \"\\\",\\\"\") + \"]\")+ \"";
+                        String printValue = "\"+("+ fname + " == null ? \"[]\" :\"[\" + com.sun.deploy.util.StringUtils.join("+ fname +", \",\").replaceAll(\",\", \"\\\",\\\"\") + \"]\")+ \"";
                         sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ printValue);
                     } else {
                         isCollectionType = false;
@@ -85,19 +89,40 @@ public class ToStringGen extends AnAction {
 
                 //不是集合类型的属性
                 if(!isCollectionType){
+
                     //数组类型的属性
                     if(fieldClassType.contains("[]")){
                         String printValue = "[\\\"\" + StringUtils.join(java.util.Arrays.asList("+ fname +" == null ? new Object[]{} : "+ fname +"), \",\").replaceAll(\",\", \"\\\",\\\"\") +\"\\\"]";
                         sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ printValue);
-                    } else {
+
+                    //简单字符串类型
+                    } else if("String".equals(fieldClassType)){
                         String printValue = fname + " == null ? null : \"\\\"\" + "+ fname +" + \"\\\"\"";
                         sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ "\" + ("+ printValue +") +\"");
+
+                    //键值对类型,只有这一种类型的属性依赖JSONObject的jar
+                    } else {
+                        boolean isKvType = true;
+                        try {
+                            //是否从Map中继承
+                            if(Map.class.isAssignableFrom(Class.forName(classType))){
+                                String printValue = "\"+com.alibaba.fastjson.JSONObject.toJSON("+ fname +" == null ? new Object() : "+ fname +")+\"";
+                                sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ printValue);
+                            } else {
+                                isKvType = false;
+                            }
+                        } catch (ClassNotFoundException e) {
+                            isKvType = false;
+                        }
+                        //其它定义对象类型
+                        if(!isKvType){
+                            //自定义对象统一走自身的toString，尽可能避免用户装 com.alibaba.fastjson.JSONObject的依赖
+                            //String printValue = fname + " == null ? null : \"\\\"\" + "+ fname +" + \"\\\"\"";
+                            String printValue = fname + " == null ? null : " +  fname;
+                            sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ "\" + ("+ printValue +") +\"");
+                        }
                     }
                 }
-
-            //数值类型的属性
-            } else {
-                sb.append((fieldCount != 0 ? "," : "") + "\\\"" + fname + "\\\"" + " : "+ "\" + "+ fname +" +\"");
             }
             fieldCount++;
         }
